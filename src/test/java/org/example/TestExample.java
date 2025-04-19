a01446b6b83d1ac00d6e66cfb1f4ca5590e16572
sgao <sgao@beckman.com>
2025-04-19 17:13:32 +0800

    fix

---
+++ src/test/java/org/example/TestExample.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestExample {
+
+    @Test
+    public void testInt() {
+        Assertions.assertEquals(0, 0);
+    }
+
+    @Test
+    public void testLong() {
+        Assertions.assertEquals(0L, 0L);
+    }
+}
